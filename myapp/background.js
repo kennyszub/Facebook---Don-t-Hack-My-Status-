50b257ae07360c869d0ff52f97801d3a600ac057
Ken Joh Szubzda <k.szubzda@berkeley.edu>
2012-12-28 19:31:06 -0500

    fixed toggle bug

---
+++ myapp/background.js
@@ -0,0 +1,44 @@
+// Contains method for array
+Array.prototype.contains = function ( needle ) {
+   for (i in this) {
+       if (this[i] == needle) return true;
+   }
+   return false;
+}
+
+queryInfo = new Object();
+queryInfo.url = "https://www.facebook.com/*";
+
+// Array of tabIds where page action has been clicked
+tabIds = [];
+
+// Show icon only on Facebook tabs
+function checkForValidUrl(tabId, changeInfo, tab) {
+    if (tab.url.search("https://www.facebook.com/*") != -1) {
+        chrome.pageAction.show(tabId);
+    }
+}
+
+// Listen for any changes to the URL of a tab
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+// InsertCSS to show or hide Post button
+chrome.pageAction.onClicked.addListener(function(tab) {
+    if (tabIds.contains(tab.id)) {
+        // hide Post button
+        chrome.tabs.insertCSS(null, 
+                         {code:".uiButtonConfirm {visibility:hidden !important;}"});
+        var index = tabIds.indexOf(tab.id);
+        tabIds.splice(index, 1);
+    } else {
+        // show Post button
+        chrome.tabs.insertCSS(null, 
+                         {code:".uiButtonConfirm {visibility:visible !important;}"});
+        tabIds.push(tab.id);
+    }
+}
+    
+);
+
+
+
